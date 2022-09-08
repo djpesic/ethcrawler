14be33791de7b4b2d409ae3a5fb7ee518216f43d
djordje.pesic <djpesic@yahoo.com>
2022-09-08 23:08:53 +0200

    Send input data to server.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use warp::Filter;
-use std::fs;
+use std::{fs, collections::HashMap};
 
 #[tokio::main]
 async fn main() {
-    // GET /crawler => 200 OK with body main_page.html
+    
+
     let main_page = warp::path!("crawler")
         .map(||{warp::reply::html(fs::read_to_string("res/main_page.html").unwrap())});
 
-    warp::serve(main_page)
+    let get_transactions = warp::path!("crawler" / "get_transactions").
+    and(warp::body::form()).
+    map(|simple_map: HashMap<String, String>|{
+        println!("Received: {:?}", simple_map);
+        ""
+    });
+    
+    let final_routes = main_page.or(get_transactions);
+
+    warp::serve(final_routes)
         .run(([127, 0, 0, 1], 3030))
         .await;
 }
